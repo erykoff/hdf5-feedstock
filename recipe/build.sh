80234a4d87947d9981d6491672a5077093375ca7
Eli Rykoff <erykoff@stanford.edu>
2020-12-27 22:26:26 -0800

    Add configure and Makefile patches to enable arm64 cross-compile.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./bin
 
 #!/bin/bash
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./bin
+
 
 export LIBRARY_PATH="${PREFIX}/lib"
 
@@ -24,6 +26,34 @@ else
   export FC=$(basename ${FC})
 fi
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 && $target_platform == "osx-arm64" ]]; then
+  export ac_cv_sizeof_long_double=8
+  export hdf5_cv_ldouble_to_long_special=no
+  export hdf5_cv_long_to_ldouble_special=no
+  export hdf5_cv_ldouble_to_llong_accurate=yes
+  export hdf5_cv_llong_to_ldouble_correct=yes
+  export hdf5_cv_disable_some_ldouble_conv=no
+  export hdf5_cv_system_scope_threads=yes
+  export hdf5_cv_printf_ll="l"
+  export PAC_FC_MAX_REAL_PRECISION=15
+  export PAC_C_MAX_REAL_PRECISION=17
+  export PAC_FC_ALL_INTEGER_KINDS="{1,2,4,8,16}"
+  export PAC_FC_ALL_REAL_KINDS="{4,8}"
+  export H5CONFIG_F_NUM_RKIND="INTEGER, PARAMETER :: num_rkinds = 2"
+  export H5CONFIG_F_NUM_IKIND="INTEGER, PARAMETER :: num_ikinds = 5"
+  export H5CONFIG_F_RKIND="INTEGER, DIMENSION(1:num_rkinds) :: rkind = (/4,8/)"
+  export H5CONFIG_F_IKIND="INTEGER, DIMENSION(1:num_ikinds) :: ikind = (/1,2,4,8,16/)"
+  export PAC_FORTRAN_NATIVE_INTEGER_SIZEOF="                    4"
+  export PAC_FORTRAN_NATIVE_INTEGER_KIND="           4"
+  export PAC_FORTRAN_NATIVE_REAL_SIZEOF="                    4"
+  export PAC_FORTRAN_NATIVE_REAL_KIND="           4"
+  export PAC_FORTRAN_NATIVE_DOUBLE_SIZEOF="                    8"
+  export PAC_FORTRAN_NATIVE_DOUBLE_KIND="           8"
+  export PAC_FORTRAN_NUM_INTEGER_KINDS="5"
+  export PAC_FC_ALL_REAL_KINDS_SIZEOF="{4,8}"
+  export PAC_FC_ALL_INTEGER_KINDS_SIZEOF="{1,2,4,8,16}"
+fi
+
 ./configure --prefix="${PREFIX}" \
             ${CONFIGURE_ARGS} \
             --with-pic \
@@ -43,6 +73,21 @@ fi
 
 # allow oversubscribing with openmpi in make check
 export OMPI_MCA_rmaps_base_oversubscribe=yes
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+  (
+    # Make a native build of the hdetect and H5make_libsettings executables
+    mkdir -p native-build/bin
+    pushd native-build/bin
+
+    $CC_FOR_BUILD ../../src/H5detect.c -I ../../src/ -o H5detect
+    $CC_FOR_BUILD ../../src/H5make_libsettings.c -I ../../src/ -o H5make_libsettings
+    $CC_FOR_BUILD ../../fortran/src/H5match_types.c -I ../../src/ -o H5match_types
+    $FC_FOR_BUILD ../../fortran/src/H5_buildiface.F90 -I ../../fortran/src/ $LDFLAGS -o H5_buildiface
+
+    popd
+  )
+fi
 
 if [[ "$CI" != "travis" ]]; then
   make -j "${CPU_COUNT}" ${VERBOSE_AT}
